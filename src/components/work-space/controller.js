d829228372f12f6bcab406c23c083f9b68baa4bc
Artyom Savchenko <savchenko.artyom.v@gmail.com>
2020-01-03 14:21:46 +0600

    Add work space component.

---
+++ src/components/work-space/controller.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import BoardsList from '../boards-list'; 
+
+
+const controller = () => {
+
+    return (
+        <BoardsList />
+    );
+};
+
+
+export default controller;
