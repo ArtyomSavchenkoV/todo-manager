949495a6c5b040c23b2fe0ba2558ce1d7374b656
Artyom Savchenko <savchenko.artyom.v@gmail.com>
2020-01-03 16:01:25 +0600

    Add board item component.

--- src/components/boards-list/controller.js
+++ src/components/boards-list/controller.js
@@ -1,14 +1,35 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import compose from '../../utils/compose';
 
 import Layout from './views/layout';
+import BoardItem from './views/board-item';
 
+const Controller = ({
+    boardsStore
+}) => {
+    const {
+        boards: boardsDataArray
+    } = boardsStore;
 
-const Controller = () => {
+    const boardsElements = boardsDataArray.map((el) => {
+        return <BoardItem key={el.id} />
+    });
 
     return (
-        <Layout />
+        <Layout
+            boardsElements={boardsElements}
+        />
     );
 };
 
 
-export default Controller;
+const mapStoreToProps = ({ boardsStore }) => {
+    return {
+        boardsStore
+    }
+}
+
+export default compose(
+    connect(mapStoreToProps)
+)(Controller);
---
+++ src/components/boards-list/views/board-item/board-item.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './board-item.scss';
+
+
+const BoardItem = () => {
+    return (
+        <div className="board-item">
+        </div>
+    );
+};
+
+
+export default BoardItem;
