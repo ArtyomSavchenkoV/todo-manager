26d0d5f937bf03b2a72b871500069485ebdd5e5b
Artyom Savchenko <savchenko.artyom.v@gmail.com>
2020-01-03 14:57:28 +0600

    Add boards list component.

---
+++ src/components/boards-list/controller.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+const Controller = () => {
+
+    return (
+
+    );
+};
+
+
+export default Controller;
---
+++ src/components/boards-list/views/layout/layout.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import './layout.scss';
+
+
+const Layout = () => {
+
+    return (
+        <div className="boards-list">
+            ##Hardcode boards-list
+        </div>
+    );
+};
+
+
+export default Layout;
