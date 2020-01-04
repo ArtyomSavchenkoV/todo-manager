7001a3f1a576e2ae552360c170dcf71f18b4b1aa
Artyom Savchenko <savchenko.artyom.v@gmail.com>
2020-01-04 17:45:23 +0600

    Add todo-board component.

--- src/components/work-space/controller.js
+++ src/components/work-space/controller.js
@@ -1,12 +1,13 @@
 import React from 'react';
 
 import BoardsList from '../boards-list'; 
+import TodoBoard from '../todo-board';
 
 
 const Controller = () => {
 
     return (
-        <BoardsList />
+        <TodoBoard />
     );
 };
 
