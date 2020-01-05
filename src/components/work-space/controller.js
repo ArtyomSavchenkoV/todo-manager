226d143597f914d6b67523758d18f3646883f71c
Artyom Savchenko <savchenko.artyom.v@gmail.com>
2020-01-05 19:43:31 +0600

    Add direct route to todo-list.

--- src/components/work-space/controller.js
+++ src/components/work-space/controller.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 import BoardsList from '../boards-list'; 
 import TodoBoard from '../todo-board';
+import TodoList from '../todo-list';
 
 
 const Controller = () => {
@@ -17,7 +18,12 @@ const Controller = () => {
                 />
                 <Route
                     path='/:id?'
+                    exact
                     render={({ match }) => <TodoBoard boardId={Number.parseInt(match.params.id, 10)}/>}
+                />
+                <Route
+                    path='/:boardId/:listId?'
+                    render={({ match }) => <TodoList boardId={Number.parseInt(match.params.id, 10)}  listId={Number.parseInt(match.params.id, 10)}/>}
                 />
                 <Route
                     render={() => <h2>404 - page not found.</h2>}
