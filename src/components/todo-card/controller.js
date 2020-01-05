8d45bd2040bb256b5c3e601df04747e2ac0a7973
Artyom Savchenko <savchenko.artyom.v@gmail.com>
2020-01-05 23:04:39 +0600

    [WIP] Add todo-card component.

---
+++ src/components/todo-card/controller.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import Layout from './views/layout';
+
+
+const Controller = () => {
+
+    return (
+        <layout />
+    );
+};
+
+
+export default Controller;
---
+++ src/components/todo-card/views/layout/layout.js
@@ -0,0 +1,16 @@
+import React from './react';
+
+import './layout.scss';
+
+
+const layout = () => {
+
+    return (
+        <div className="todo-card">
+            ##Card
+        </div>
+    );
+};
+
+
+export default Layout;
