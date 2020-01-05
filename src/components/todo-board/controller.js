8db951c578be516eef187646996b3eea5021a6a1
Artyom Savchenko <savchenko.artyom.v@gmail.com>
2020-01-05 15:54:14 +0600

    Include todo-list component.

--- src/components/todo-board/controller.js
+++ src/components/todo-board/controller.js
@@ -1,15 +1,32 @@
 import React from 'react';
 
+import TodoList from '../todo-list';
+
 import Layout from './views/layout';
 import Header from './views/header';
 import NewListForm from './new-list-form';
 
 
-const Controller = () => {
+const Controller = ({
+    boardId
+}) => {
 
     const header = <Header />;
 
-    let todoListsElements = [];
+    const dummy = [
+        {listId: 1},
+        {listId: 2},
+        {listId: 3},
+        {listId: 4},
+        {listId: 5}
+    ];
+
+    let todoListsElements = dummy.map((el) => {
+        return (
+            <TodoList key={el.listId} listId={el.listId} boardId={boardId} />
+        );
+    });
+
     todoListsElements.push({
         element: <NewListForm onConfirm={()=>{}}/>,
         key: 'new-list-form'
