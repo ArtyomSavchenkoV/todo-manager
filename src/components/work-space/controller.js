a657e6e661d5982840da6aff45a684126c6f924f
Artyom Savchenko <savchenko.artyom.v@gmail.com>
2020-01-05 23:12:43 +0600

    Add using route for todo-card.

--- src/components/work-space/controller.js
+++ src/components/work-space/controller.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import BoardsList from '../boards-list'; 
 import TodoBoard from '../todo-board';
 import TodoList from '../todo-list';
+import TodoCard from '../todo-card';
 
 
 const Controller = () => {
@@ -23,7 +24,18 @@ const Controller = () => {
                 />
                 <Route
                     path='/:boardId/:listId?'
-                    render={({ match }) => <TodoList boardId={Number.parseInt(match.params.id, 10)}  listId={Number.parseInt(match.params.id, 10)}/>}
+                    exact
+                    render={({ match }) => <TodoList boardId={Number.parseInt(match.params.boardId, 10)}  listId={Number.parseInt(match.params.listId, 10)}/>}
+                />
+                <Route
+                    path='/:boardId/:listId/:cardId?'
+                    render={({ match }) => (
+                        <TodoCard 
+                            boardId={Number.parseInt(match.params.boardId, 10)}  
+                            listId={Number.parseInt(match.params.listId, 10)}
+                            cardId={Number.parseInt(match.params.cardId, 10)}
+                        />
+                    )}
                 />
                 <Route
                     render={() => <h2>404 - page not found.</h2>}
