77938f217943388847accab29d3bc2f69e69f287
Artyom Savchenko <savchenko.artyom.v@gmail.com>
2020-01-06 00:42:14 +0600

    [WIP] Make todo-card-form component.

--- src/components/todo-card-form/controller.js
+++ src/components/todo-card-form/controller.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { withLocalizationService } from '../hoc';
 
 import Layout from './views/layout';
 
@@ -6,13 +7,26 @@ import Layout from './views/layout';
 class Controller extends Component {
 
     render() {
+        const {
+            localize
+        } = this.props;
 
         return (
-            <Layout 
+            <Layout
+                titleLocalizedText={localize('todoCardForm.titleLocalizedText')}
+                cardNameLocalizedText={localize('todoCardForm.cardNameLocalizedText')}
+                descriptionLocalizedText={localize('todoCardForm.descriptionLocalizedText')}
+                stateLocalizedText={localize('todoCardForm.stateLocalizedText')}
+
+                confirmLocalizedText={localize('todoCardForm.confirmLocalizedText')}
+                cancelLocalizedText={localize('todoCardForm.cancelLocalizedText')}
+
+                onConfirm={()=>{}}
+                onCancel={()=>{}}
             />
         );
     };
 };
 
 
-export default Controller;
+export default withLocalizationService(Controller);
