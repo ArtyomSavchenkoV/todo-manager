731eb2dcd8c6fcb2e50de006534d8fb6dde8e4cb
Artyom Savchenko <savchenko.artyom.v@gmail.com>
2020-01-06 21:03:01 +0600

    Add todo card data validators.

---
+++ src/utils/validators/index.js
@@ -0,0 +1,9 @@
+import itemNameValidator from './item-name-validator';
+import itemDescriptionValidator from './item-description-validator';
+import cardStateValidator from './card-state-validator';
+
+export {
+    itemNameValidator,
+    itemDescriptionValidator,
+    cardStateValidator
+};
