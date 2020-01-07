c0d26b8ba7215412a74263746e9a29e77b47aca7
Artyom Savchenko <savchenko.artyom.v@gmail.com>
2020-01-07 13:16:34 +0600

    Add card-data fetcher.

--- src/components/todo-card/controller.js
+++ src/components/todo-card/controller.js
@@ -1,10 +1,20 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import compose from '../../utils/compose';
+import fetchObjectFromArrayById from '../../utils/fetch-object-from-array-by-id';
 
 import Layout from './views/layout';
 import CardState from './views/card-state';
 
 
-const Controller = () => {
+const Controller = ({
+    boardId,
+    listId,
+    cardId,
+
+    boardsList
+}) => {
+    const cardData = fetchData(boardsList, boardId, listId, cardId);
 
     const cardState = <CardState cardState="" />
 
@@ -20,7 +30,38 @@ const Controller = () => {
 };
 
 
-export default Controller;
+const mapStoreToProps = ({ boardsList }) => {
+    return {
+        boardsList
+    };
+};
+
+export default compose(
+    connect(mapStoreToProps)
+)(Controller);
+
+
+/*
+*   Fetch data from store `cardData`  
+*/
+const fetchData = (boardsList, boardId, listId, cardId) => {   
+    const currentBoard = fetchObjectFromArrayById(boardsList.boards, boardId);    
+    if (currentBoard === false) {
+        return false;
+    }
+
+    const currentTodoList = fetchObjectFromArrayById(currentBoard.todoLists, listId); 
+    if (currentTodoList === false) {
+        return false;
+    }
+
+    const currentCardData = fetchObjectFromArrayById(currentTodoList.todoCards, cardId); 
+    if (currentCardData === false) {
+        return false;
+    }
+
+    return currentCardData;
+};
 
 /*
 id: 1
